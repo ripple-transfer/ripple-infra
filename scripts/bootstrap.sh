c891ff718922f5b70f8cc674c01bae71fd0e0eca
Zsolt Paral <paralzsolt@gmail.com>
2023-10-15 16:16:55 +0200

    (feat) Switch to linkerd

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,2 +1,3 @@
+kind create cluster --config kind-config.yaml
 flux bootstrap github --token-auth --owner=loom-lang --repository=loom-infra --branch master --path=clusters/dev
 cat ./.cert/age.agekey | kubectl create secret generic sops-age --namespace flux-system --from-file=age.agekey=/dev/stdin
