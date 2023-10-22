31b9b7ee454aa36dd8c238068c0eabcf1c5a9726
Zsolt Paral <paralzsolt@gmail.com>
2023-10-22 21:10:47 +0200

    (feat) Use cilium

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,3 +1,6 @@
 kind create cluster --config kind-config.yaml
+helm repo add cilium https://helm.cilium.io/
+helm install cilium cilium/cilium --namespace kube-system -f ./infrastructure/cilium/values.yaml
+cilium status --wait
 flux bootstrap github --token-auth --owner=loom-lang --repository=loom-infra --branch master --path=clusters/dev
 cat ./.cert/age.agekey | kubectl create secret generic sops-age --namespace flux-system --from-file=age.agekey=/dev/stdin
