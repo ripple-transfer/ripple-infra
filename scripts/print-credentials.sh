912beacae80504ce2a7e2d2994562d55290090e3
zsparal <paralzsolt@gmail.com>
2024-04-23 08:32:51 +0200

    (feat) Deploy Mimir

--- scripts/print-credentials.sh
+++ scripts/print-credentials.sh
@@ -4,5 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Minio: $(kubectl get secret -n minio minio-root -o jsonpath='{.data.root-user}' | base64 -d) / $(kubectl get secret -n minio minio-root -o jsonpath='{.data.root-password}' | base64 -d)"
-echo "K8s dashboard: $(kubectl get secret -n kubernetes-dashboard dashboard-admin-token -o jsonpath='{.data.token}' | base64 -d)"
+MINIO_SA_TOKEN=$(kubectl -n minio get secret console-sa-secret -o jsonpath="{.data.token}" | base64 --decode)
+
+echo "Minio: ${MINIO_SA_TOKEN}"
+# echo "K8s dashboard: $(kubectl get secret -n kubernetes-dashboard dashboard-admin-token -o jsonpath='{.data.token}' | base64 -d)"
